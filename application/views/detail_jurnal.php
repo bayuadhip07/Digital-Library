010edd6e827eda6beb21efbe02d8dc12ed6c6272
rensan <rendymasamune@gmail.com>
2020-04-14 21:38:44 +0700

    ambil update baru

---
+++ application/views/detail_jurnal.php
@@ -0,0 +1,100 @@
+<<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php
+		$this->load->view('include/head');
+	?>
+
+</head>
+
+<body>
+	
+	<!-- <div id="preloader">
+		<div data-loader="circle-side"></div>
+	</div> -->
+	<!-- End Preload -->
+	
+	<header class="header fadeInDown">
+		<?php
+			$this->load->view('include/header');
+		?>
+	</header>
+	<!-- /header -->
+	
+	<main>
+		<section id="hero_in" class="courses">
+			<div class="wrapper">
+				<div class="container">
+					<h1 class="fadeInUp"><span></span>Deskripsi Journal</h1>
+				</div>
+			</div>
+		</section>
+		<!--/hero_in-->
+			<!-- /container -->
+		<!-- /filters -->
+
+		<div class="bg_color_1">
+			<nav class="secondary_nav sticky_horizontal">
+				<div class="container">
+					<ul class="clearfix">
+						<li><a href="#description" class="active">Description</a></li>
+
+					</ul>
+				</div>
+			</nav>
+			<div class="container margin_60_35">
+				<div class="row">
+					<div class="col-lg-8">
+						
+						<section id="description">
+							<h2>Description</h2>
+							<p>Per consequat adolescens ex, cu nibh commune temporibus vim, ad sumo viris eloquentiam sed. Mea appareat omittantur eloquentiam ad, nam ei quas oportere democritum. Prima causae admodum id est, ei timeam inimicus sed. Sit an meis aliquam, cetero inermis vel ut. An sit illum euismod facilisis, tamquam vulputate pertinacia eum at.</p>
+							<h5>What will you learn</h5>
+							<ul class="list_ok">
+								<li>
+									<h6>Suas summo id sed erat erant oporteat</h6>
+									<p>Ut unum diceret eos, mel cu velit principes, ut quo inani dolorem mediocritatem. Mea in justo posidonium necessitatibus.</p>
+								</li>
+								<li>
+									<h6>Illud singulis indoctum ad sed</h6>
+									<p>Ut unum diceret eos, mel cu velit principes, ut quo inani dolorem mediocritatem. Mea in justo posidonium necessitatibus.</p>
+								</li>
+								<li>
+									<h6>Alterum bonorum mentitum an mel</h6>
+									<p>Ut unum diceret eos, mel cu velit principes, ut quo inani dolorem mediocritatem. Mea in justo posidonium necessitatibus.</p>
+								</li>
+							</ul>
+							<hr>
+								</div>
+							</div>
+							<!-- /row -->
+						</section>
+						<!-- /section -->
+						<aside class="col-lg-4" id="sidebar">
+						<div class="box_detail">
+							<a href="cart-1.html" class="btn_1 full-width">Download PDF</a>
+						</div>
+					</aside>
+						
+	<!--/footer-->
+	
+	<!-- Search Menu -->
+	<div class="search-overlay-menu">
+		<span class="search-overlay-close"><span class="closebt"><i class="ti-close"></i></span></span>
+		<form role="search" id="searchform" method="get">
+			<input value="" name="q" type="search" placeholder="Search..." />
+			<button type="submit"><i class="icon_search"></i>
+			</button>
+		</form>
+	</div><!-- / Search Menu -->
+	
+	<!-- COMMON SCRIPTS -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <script src="js/common_scripts.js"></script>
+    <script src="js/main.js"></script>
+	<script src="assets/validate.js"></script>
+	
+  
+</body>
+</html>
